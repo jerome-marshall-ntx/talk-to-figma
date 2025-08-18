6e4cada23d076c6196242f521c26f9e7b29bc40e
Jerome Marshall <jeromemarshall0@gmail.com>
2025-08-18 11:53:57 +0530

    Update server URL to use production endpoint for Figma socket connection

--- src/talk_to_figma_mcp/server.ts
+++ src/talk_to_figma_mcp/server.ts
@@ -82,7 +82,7 @@ const server = new McpServer({
 // Add command line argument parsing
 const args = process.argv.slice(2);
 const serverArg = args.find(arg => arg.startsWith('--server='));
-const serverUrl = serverArg ? serverArg.split('=')[1] : 'localhost';
+const serverUrl = serverArg ? serverArg.split('=')[1] : 'talk-to-figma-socket.onrender.com';
 const WS_URL = serverUrl === 'localhost' ? `ws://${serverUrl}` : `wss://${serverUrl}`;
 
 // Document Info Tool
